207a3dfd8ebb6ce7c0b27a90c021cce55b52a6d1
Mazd3 <haziullin2000@gmail.com>
2025-01-02 20:39:06 +0500

    fix

--- src/components/layout-link-list.tsx
+++ src/components/layout-link-list.tsx
@@ -1,13 +1,3 @@
-import {
-  IconBrandDiscord,
-  IconBrandGithub,
-  IconBrandSpotify,
-  IconBrandSteam,
-  IconBrandTelegram,
-} from "@tabler/icons-react";
-
-import { LayoutLink } from "./layout-link";
-
 import styles from "./layout-link-list.module.css";
 
 export const LayoutLinkList: React.FC<{ children: React.ReactNode }> = ({
